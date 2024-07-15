30f32d9740d33659d62507ef160f80a26a44495c
akkas_dev <codersadhin@gmail.com>
2024-07-15 21:02:27 +0800

    vector_queries problem solved

---
+++ level_one/vector_queries.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int q;
+    cin >> q;
+    vector<int> nums;
+
+    while (q--) {
+        int x,val;
+        cin >> x >> val;
+
+        if (x == 1) {
+            if (nums.empty()) {
+                nums.push_back(val);
+            } else {
+                nums.insert((nums.begin() + 1), val);
+            }
+        } else if (x == 2) {
+            if (nums.empty()) {
+                nums.push_back(val);
+            } else {
+                nums.insert((nums.end() - 1), val);
+            }
+        }
+    }
+
+    for (int num : nums) {
+        cout << num << " ";
+    }
+
+    return 0;
+}
