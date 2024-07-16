15aa36c8aa6b6dfb1c6da37bcb8370658f13fff6
akkas_dev <codersadhin@gmail.com>
2024-07-16 23:52:29 +0800

    right side max value create a function

--- level_one/rightSide_max_value.cpp
+++ level_one/rightSide_max_value.cpp
@@ -1,20 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// void rightMaxValue(vector<int> nums, int p){
-//     if(nums.size() == p) return;
-//     int val=nums[p+1];
-//     for (int i = p+1; i < nums.size(); i++)
-//     {
-//         val=max(val,nums[i]);
-//     }
-//     if(p==nums.size()-1){
-//         cout<<0;
-//     }else{
-//         cout<<val<<" ";
-//     }
-//     rightMaxValue(nums,p+1);
-// }
+void rightMaxValue(vector<int> nums, int p){
+    if(nums.size() == p) return;
+    int val=nums[p+1];
+    for (int i = p+1; i < nums.size(); i++)
+    {
+        val=max(val,nums[i]);
+    }
+    if(p==nums.size()-1){
+        cout<<0;
+    }else{
+        cout<<val<<" ";
+    }
+    // rightMaxValue(nums,p+1);
+}
 
 int main() {
     vector<int> nums;
