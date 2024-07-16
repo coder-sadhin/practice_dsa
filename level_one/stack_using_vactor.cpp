243981f1dff9698ae583478b978a7fb9dee9c172
akkas_dev <codersadhin@gmail.com>
2024-07-16 23:34:04 +0800

    stack using vector push value using for loop

--- level_one/stack_using_vactor.cpp
+++ level_one/stack_using_vactor.cpp
@@ -23,11 +23,11 @@ int main() {
     int n;
     cin >> n;
     
-    // for (int i = 0; i < n; i++) {
-    //     int value;
-    //     cin >> value;
-    //     s.push(value);
-    // }
+    for (int i = 0; i < n; i++) {
+        int value;
+        cin >> value;
+        s.push(value);
+    }
     // int k;
     // cin>>k;
     // cout<<s.kthValue(k,n)<<endl;
